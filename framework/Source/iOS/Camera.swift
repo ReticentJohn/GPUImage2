902186e53f37803d0bd71ef38357e3e84ae1605a
John Gabelmann <john@floof.dog>
2023-10-23 14:36:14 -0400

    Fix main thread hang on capture session
    Signed-off-by: John Gabelmann <john@floof.dog>

--- framework/Source/iOS/Camera.swift
+++ framework/Source/iOS/Camera.swift
@@ -283,7 +283,9 @@ public class Camera: NSObject, ImageSource, AVCaptureVideoDataOutputSampleBuffer
         self.totalFrameTimeDuringCapture = 0
         
         if (!captureSession.isRunning) {
-            captureSession.startRunning()
+            DispatchQueue.global(qos: .background).async {
+                self.captureSession.startRunning()
+            }
         }
     }
     
